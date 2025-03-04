3ba916b4f41f8cdd0b0c5f258d82eecde6d8c6b0
Prathyusha Kurigala <prathyushakiran@Prathyushas-MBP.attlocal.net>
2025-03-03 17:20:03 -0800

    day-68 python advance authentication with flask

--- day-68-starting-files-flask-auth/main.py
+++ day-68-starting-files-flask-auth/main.py
@@ -8,6 +8,9 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret-key-goes-here'
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
@@ -16,15 +19,17 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
+@login_manager.user_loader
+def load_user(user_id):
+    return db.get_or_404(User, user_id)
+
+
 # CREATE TABLE IN DB
-
-
-class User(db.Model):
+class User(UserMixin, db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     email: Mapped[str] = mapped_column(String(100), unique=True)
     password: Mapped[str] = mapped_column(String(100))
     name: Mapped[str] = mapped_column(String(1000))
-
 
 with app.app_context():
     db.create_all()
@@ -32,39 +37,79 @@ with app.app_context():
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", logged_in=current_user.is_authenticated)
 
 
 @app.route('/register',methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
+        email=request.form.get('email')
+        result = db.session.execute(db.select(User).where(User.email == email))
+        user = result.scalar()
+        if user:
+             # already exists
+            flash("You've already signed up with that email, log in instead.")
+            return redirect(url_for('login'))
+
+        # salting and hashing the password entered by the user
+        hash_and_salted_password = generate_password_hash(
+            request.form.get('password'),
+            method='pbkdf2:sha256',
+            salt_length=8)
+        # storing the hashed password in our database
         new_user = User(
             email=request.form.get('email'),
-            password=request.form.get('password'),
+            password=hash_and_salted_password,
             name=request.form.get('name')
         )
         db.session.add(new_user)
         db.session.commit()
-        return render_template('secrets.html', user=new_user)
-    return render_template("register.html")
+        login_user(new_user)
+        return redirect(url_for('secrets'))
+    return render_template("register.html", logged_in=current_user.is_authenticated)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    # form = LoginForm()
+    if request.method == 'POST':
+        email=request.form.get('email')
+        password=request.form.get('password')
+
+        # find user by email entered
+        result = db.session.execute(db.select(User).where(User.email == email))
+        user = result.scalar()
+
+        # email doesnt exists nad password incorrect case
+        if not user:
+            flash('That email doesnt exists, please try again.')
+            return redirect(url_for('login'))
+        # check stored password hash against entered password hashed
+        elif not check_password_hash(user.password, password):
+            flash('Password Incorrect, please try again.')
+            return redirect(url_for('login'))
+        else:
+            login_user(user)
+            return redirect(url_for('secrets'))
+
+    return render_template("login.html", logged_in=current_user.is_authenticated)
 
 
 @app.route('/secrets')
+@login_required
 def secrets():
-    return render_template("secrets.html")
+    print(current_user.name)
+    return render_template("secrets.html", name=current_user.name, logged_in=True)
 
 
 @app.route('/logout')
 def logout():
-    pass
+    logout_user()
+    return redirect(url_for('home'))
 
 
-@app.route('/download')
+@app.route('/download', methods=['GET', 'POST'])
+@login_required
 def download():
     return send_from_directory(
         'static', path='files/cheat_sheet.pdf'
